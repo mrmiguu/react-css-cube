b28c780c793d146ba8a5930cfcf99bb5187d37e8
Alex <a.matulionis@usa.com>
2018-12-26 21:02:36 -0800

    Added CSS Cube base.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './styles/App.css'
+
+const sides = [
+  'front',
+  'right',
+  'back',
+  'left',
+  'top',
+  'bottom'
+]
 
 class App extends Component {
+
+  state = {}
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    const { side } = this.state
+    
+    return <>
+
+      <div className="scene">
+        <div className={`cube ${side || sides[0]}`}>
+          <div className="face front">FRO</div>
+          <div className="face back">BAC</div>
+          <div className="face right">RIG</div>
+          <div className="face left">LEF</div>
+          <div className="face top">TOP</div>
+          <div className="face bottom">BOT</div>
+        </div>
       </div>
-    );
+
+      <p className="radio-group">
+        {sides.map(side => (
+            <input
+              key={side}
+              type="radio"
+              name="rotate-cube-side"
+              onChange={() => this.setState({ side })}
+            />
+        ))}
+      </p>
+
+    </>
   }
 }
 
-export default App;
+export default App
